18e7e5f02c081ce151e1623f6288aed2afa21c49
Shehbaz-Niazi <163367421+Shehbaz-Niazi@users.noreply.github.com>
2024-03-25 18:51:55 -0700

    Word Counter
    A robust word counter utility crafted with TypeScript and Node.js, ideal for beginners seeking a simple yet effective tool for analyzing word frequencies and document lengths.

---
+++ app.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+let sleep = () => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 5000);
+    });
+};
+let design = async () => {
+    let animation = chalkAnimation.pulse(`Simple Word Counter--||--Created By Typescript And Nodejs`);
+    await sleep();
+    animation.stop();
+};
+await design();
+let answers = await inquirer.prompt([
+    {
+        name: "Counter",
+        type: "input",
+        message: chalk.green.bold("Please Input Any Sentence"),
+    }
+]);
+let result = answers.Counter.trim().split(" ");
+console.log(result);
+console.log(chalk.blue(`Your Sentence Length is ${result.length}`));
